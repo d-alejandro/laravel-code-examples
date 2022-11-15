3db144abbe9faefdc35029778838ce544e881001
Alexander Dadyka <sanekokokok@gmail.com>
2022-11-15 17:51:13 +0300

    Fix: renamed constructor argument to IndexOrderRequestDTO

--- app/DTO/AdminPanel/Order/IndexOrderRequestDTO.php
+++ app/DTO/AdminPanel/Order/IndexOrderRequestDTO.php
@@ -10,15 +10,15 @@ class IndexOrderRequestDTO
 {
     public function __construct(
         public readonly IndexPaginationRequestDTO $indexPaginationRequestDTO,
-        public readonly ?Carbon      $rentalDate = null,
-        public readonly ?string      $transportName = null,
-        public readonly ?string      $agencyName = null,
-        public readonly ?Carbon      $startRentalDate = null,
-        public readonly ?Carbon      $endRentalDate = null,
-        public readonly ?OrderStatus $status = null,
-        public readonly ?bool        $isConfirmed = null,
-        public readonly ?bool        $isChecked = null,
-        public readonly ?bool        $hasAdminNote = null
+        public readonly ?Carbon                   $rentalDate = null,
+        public readonly ?string                   $userName = null,
+        public readonly ?string                   $agencyName = null,
+        public readonly ?Carbon                   $startRentalDate = null,
+        public readonly ?Carbon                   $endRentalDate = null,
+        public readonly ?OrderStatus              $status = null,
+        public readonly ?bool                     $isConfirmed = null,
+        public readonly ?bool                     $isChecked = null,
+        public readonly ?bool                     $hasAdminNote = null
     ) {
     }
 }
