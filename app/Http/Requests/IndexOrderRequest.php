5dbebdba314a6d9cd5f45f717e0e637095a3adac
Alexander Dadyka <sanekokokok@gmail.com>
2023-01-23 22:11:35 +0300

    Fix: IndexOrderRequest

--- app/Http/Requests/IndexOrderRequest.php
+++ app/Http/Requests/IndexOrderRequest.php
@@ -7,7 +7,8 @@ use App\DTO\IndexOrderRequestDTO;
 use App\Enums\OrderSortColumn;
 use App\Enums\OrderStatus;
 use App\Enums\SortType;
-use App\Helpers\Interfaces\EnumValuesToStringConverterInterface;
+use App\Helpers\Interfaces\EnumSerializerInterface;
+use App\Helpers\Interfaces\BooleanFilterInterface;
 use App\Http\Requests\QueryParams\IndexOrderQueryParam;
 use App\Http\Requests\QueryParams\Pagination;
 use App\Models\Order;
@@ -15,18 +16,8 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class IndexOrderRequest extends FormRequest
 {
-    public function __construct(
-        private EnumValuesToStringConverterInterface $converter,
-        array                                        $query = [],
-        array                                        $request = [],
-        array                                        $attributes = [],
-        array                                        $cookies = [],
-        array                                        $files = [],
-        array                                        $server = [],
-        mixed                                        $content = null
-    ) {
-        parent::__construct($query, $request, $attributes, $cookies, $files, $server, $content);
-    }
+    private BooleanFilterInterface $filter;
+    private array $data;
 
     public function authorize(): bool
     {
@@ -35,17 +26,19 @@ class IndexOrderRequest extends FormRequest
 
     public function rules(): array
     {
+        $serializer = resolve(EnumSerializerInterface::class);
+
         return [
             Pagination::START => 'required|integer|min:0',
             Pagination::END => 'required|integer|min:1',
-            Pagination::SORT_COLUMN => 'required|string|in:' . $this->converter->execute(OrderSortColumn::class),
-            Pagination::SORT_TYPE => 'required|string|in:' . $this->converter->execute(SortType::class),
+            Pagination::SORT_COLUMN => 'required|string|in:' . $serializer->execute(OrderSortColumn::class),
+            Pagination::SORT_TYPE => 'required|string|in:' . $serializer->execute(SortType::class),
             Pagination::IDS => 'sometimes|required|array|exists:' . Order::TABLE_NAME . ',' . Order::COLUMN_ID,
             Pagination::IDS . '.*' => 'required|integer|min:1',
             Order::COLUMN_RENTAL_DATE => 'sometimes|required|date',
             Order::COLUMN_IS_CONFIRMED => 'sometimes|required|string|in:true,false',
             Order::COLUMN_IS_CHECKED => 'sometimes|required|string|in:true,false',
-            Order::COLUMN_STATUS => 'sometimes|required|string|in:' . $this->converter->execute(OrderStatus::class),
+            Order::COLUMN_STATUS => 'sometimes|required|string|in:' . $serializer->execute(OrderStatus::class),
             Order::COLUMN_USER_NAME => 'sometimes|required|string',
             IndexOrderQueryParam::AGENCY_NAME => 'sometimes|required|string',
             Order::COLUMN_ADMIN_NOTE => 'sometimes|required|string|in:true,false',
@@ -56,27 +49,41 @@ class IndexOrderRequest extends FormRequest
 
     public function getValidated(): IndexOrderRequestDTO
     {
-        $data = $this->validated();
+        $this->filter = resolve(BooleanFilterInterface::class);
+
+        $this->data = $this->validated();
 
         $indexOrderPaginationDTO = new IndexOrderPaginationDTO(
-            $data[Pagination::START],
-            $data[Pagination::END],
-            OrderSortColumn::from($data[Pagination::SORT_COLUMN]),
-            SortType::from($data[Pagination::SORT_TYPE]),
-            $data[Pagination::IDS] ?? null,
+            $this->data[Pagination::START],
+            $this->data[Pagination::END],
+            OrderSortColumn::from($this->data[Pagination::SORT_COLUMN]),
+            SortType::from($this->data[Pagination::SORT_TYPE]),
+            $this->checkValue(Pagination::IDS),
         );
 
         return new IndexOrderRequestDTO(
             $indexOrderPaginationDTO,
-            $data[Order::COLUMN_RENTAL_DATE] ?? null,
-            $data[Order::COLUMN_USER_NAME] ?? null,
-            $data[IndexOrderQueryParam::AGENCY_NAME] ?? null,
-            $data[IndexOrderQueryParam::START_DATE] ?? null,
-            $data[IndexOrderQueryParam::END_DATE] ?? null,
-            $data[Order::COLUMN_STATUS] ?? null,
-            $data[Order::COLUMN_IS_CONFIRMED] ?? null,
-            $data[Order::COLUMN_IS_CHECKED] ?? null,
-            $data[Order::COLUMN_ADMIN_NOTE] ?? null,
+            $this->checkValue(Order::COLUMN_RENTAL_DATE),
+            $this->checkValue(Order::COLUMN_USER_NAME),
+            $this->checkValue(IndexOrderQueryParam::AGENCY_NAME),
+            $this->checkValue(IndexOrderQueryParam::START_DATE),
+            $this->checkValue(IndexOrderQueryParam::END_DATE),
+            OrderStatus::tryFrom($this->checkValue(Order::COLUMN_STATUS)),
+            $this->filterValue(Order::COLUMN_IS_CONFIRMED),
+            $this->filterValue(Order::COLUMN_IS_CHECKED),
+            $this->filterValue(Order::COLUMN_ADMIN_NOTE),
         );
+    }
+
+    private function checkValue(string $queryParam): mixed
+    {
+        return $this->data[$queryParam] ?? null;
+    }
+
+    private function filterValue(string $queryParam): bool|null
+    {
+        return isset($this->data[$queryParam])
+            ? $this->filter->execute($this->data[$queryParam])
+            : null;
     }
 }
