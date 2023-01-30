c605c7f8e1951e917e12d89bed48faa0fbdbfd92
Alexander Dadyka <sanekokokok@gmail.com>
2023-01-30 20:12:46 +0300

    Fix: CriteriaApplierInterface

--- app/Repositories/Criteria/Interfaces/CriteriaApplierInterface.php
+++ app/Repositories/Criteria/Interfaces/CriteriaApplierInterface.php
@@ -4,7 +4,5 @@ namespace App\Repositories\Criteria\Interfaces;
 
 interface CriteriaApplierInterface
 {
-    public function __call(string $name, array $arguments): mixed;
-
     public function addCriterion(CriterionInterface $criterion): void;
 }
