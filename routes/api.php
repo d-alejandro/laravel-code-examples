0147bb004e91500443412bae2ff478fc499bc45a
Alexander Dadyka <sanekokokok@gmail.com>
2023-02-04 19:16:05 +0300

    Add: OrderStoreController

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\Api\OrderIndexController;
+use App\Http\Controllers\Api\OrderStoreController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/orders', OrderIndexController::class)->name('order.index');
+Route::post('/orders', OrderStoreController::class)->name('order.store');
